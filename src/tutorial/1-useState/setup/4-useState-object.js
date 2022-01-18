fb2e2dfacd61cbdba962ca4a355c07fd08f3763c
mendel <mendelrubinson5@gmail.com>
2022-01-18 13:58:04 -0500

    01/18/22, done useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState();
+  
+
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random message');
+
+  const changeMessage = () => {
+    // for objects
+    // setPerson({...person, message: 'hello world'})
+    
+    setMessage('hello world')
+    // setName("mendel")
+    // setAge(17)
+  }
+
+  return <>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{message}</h3>
+  <button className='btn' onClick={changeMessage}>change message</button>
+  </>;
 };
 
 export default UseStateObject;
